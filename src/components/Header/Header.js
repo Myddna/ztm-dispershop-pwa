b8df6316d82539fee2f9041c782398345fb4209e
Myddna <myddna@dispersion.es>
2021-12-21 17:12:06 +0100

    Adding note to SVG import

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -1,12 +1,16 @@
 import { Link } from 'react-router-dom';
 import styles from './Header.module.scss';
+/**
+ * This is a CreateReactApp "special"
+ * See more: https://create-react-app.dev/docs/adding-images-fonts-and-files/#adding-svgs
+ */
 import { ReactComponent as Logo } from '../../assets/crown.svg';
 
 const Header = function () {
   return (
     <header className={styles.Header}>
       <Link to="/" className={styles.logoContainer}>
-        <Logo className="logo" />
+        <Logo className="logo" title="DisperShop Store" />
       </Link>
       <nav className={styles.options}>
         <Link to="/shop" className={styles.option}>
