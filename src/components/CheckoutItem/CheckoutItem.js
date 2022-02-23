1c8b41ece03ae79fcd7745eaeafd4546ed8fdcf3
Myddna <myddna@dispersion.es>
2022-02-23 16:07:58 +0100

    Functional increase and decrease item quantities in checkout page

--- src/components/CheckoutItem/CheckoutItem.js
+++ src/components/CheckoutItem/CheckoutItem.js
@@ -1,26 +1,58 @@
 import React from 'react';
 import PropTypes from 'prop-types';
+import { connect } from 'react-redux';
 
 import styles from './CheckoutItem.module.scss';
+import {
+  clearItem as clearItemAction,
+  addItem as addItemAction,
+  removeItem as removeItemAction,
+} from '../../redux/cart/cart.actions';
 
 const CheckoutItem = function ({
-  cartItem: {
-    id, imageUrl, price, name, quantity,
-  },
+  cartItem, clearItem, addItem, removeItem,
 }) {
-  console.log(id);
+  const {
+    imageUrl, price, name, quantity,
+  } = cartItem;
   return (
     <div className={styles.CheckoutItem}>
       <div className={styles.imageContainer}>
         <img src={imageUrl} alt={name} />
       </div>
       <span className={styles.name}>{name}</span>
-      <span className={styles.quantity}>{quantity}</span>
+      <span className={styles.quantity}>
+        <div
+          className={styles.arrow}
+          role="button"
+          tabIndex={0}
+          onClick={() => removeItem(cartItem)}
+        >
+          &#10094;
+        </div>
+        <span className={styles.value}>{quantity}</span>
+        <div
+          className={styles.arrow}
+          role="button"
+          tabIndex={0}
+          onClick={() => addItem(cartItem)}
+        >
+          &#10095;
+        </div>
+      </span>
       <span className={styles.price}>
         {price}
         €
       </span>
-      <div className={styles.removeButton}>&#10005;</div>
+      <div
+        className={styles.removeButton}
+        role="button"
+        tabIndex={0}
+        onClick={() => clearItem(cartItem)}
+      >
+        &#10005;
+
+      </div>
     </div>
   );
 };
@@ -33,10 +65,22 @@ CheckoutItem.propTypes = {
     name: PropTypes.string,
     quantity: PropTypes.number,
   }),
+  clearItem: PropTypes.func,
+  addItem: PropTypes.func,
+  removeItem: PropTypes.func,
 };
 
 CheckoutItem.defaultProps = {
   cartItem: null,
+  clearItem: () => {},
+  addItem: () => {},
+  removeItem: () => {},
 };
 
-export default CheckoutItem;
+const mapDispatchToProps = (dispatch) => ({
+  clearItem: (item) => dispatch(clearItemAction(item)),
+  addItem: (item) => dispatch(addItemAction(item)),
+  removeItem: (item) => dispatch(removeItemAction(item)),
+});
+
+export default connect(null, mapDispatchToProps)(CheckoutItem);
--- src/redux/cart/cart.utils.js
+++ src/redux/cart/cart.utils.js
@@ -9,4 +9,24 @@ export const addItemToCart = (cartItems, cartItemToAdd) => {
   return [...cartItems, { ...cartItemToAdd, quantity: 1 }];
 };
 
-export default addItemToCart;
+export const clearItemFromCart = function (cartItems, cartItemToRemove) {
+  // Filter creates a new array
+  return cartItems.filter((cartItem) => cartItem.id !== cartItemToRemove.id);
+};
+
+export const removeItemFromCart = function (cartItems, cartItemToRemove) {
+  // if we are decreasing, we know for a fact that tere is at least one element
+  const existingCartItem = cartItems.find((cartItem) => cartItem.id === cartItemToRemove.id);
+
+  // If there is only one item, remove the item entirely
+  if (existingCartItem.quantity === 1) {
+    return cartItems.filter((cartItem) => cartItem.id !== cartItemToRemove.id);
+  }
+
+  // In other case, decrease the count
+  return cartItems.map((cartItem) => (
+    cartItem.id === cartItemToRemove.id
+      ? { ...cartItem, quantity: cartItem.quantity - 1 }
+      : cartItem
+  ));
+};
